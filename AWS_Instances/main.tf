7a328dfd8bef262d37a3e2531350071ac7e13f3b
tikor6485 <tikor6485@yahoo.com>
2023-02-21 09:04:19 +0100

    AWS_Instances is finished.

---
+++ AWS_Instances/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "TirdadKor" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name       = var.machine_name
+    Created_By = "Terraform-Tirdad"
+  }
+}
