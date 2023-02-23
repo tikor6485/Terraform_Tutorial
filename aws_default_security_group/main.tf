df8c5cdbf764b5a43144754b5ae233741c981dfc
tikor6485 <tikor6485@yahoo.com>
2023-02-23 09:19:55 +0100

    aws_default_security_group is finished

---
+++ aws_default_security_group/main.tf
@@ -0,0 +1,70 @@
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "myapp-subnet-01" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subet_cidr_block
+  availability_zone = var.avail_zone
+
+  tags = {
+    Name = "${var.env_prefix}-subnet-01"
+  }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
+}
+
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
+
+}
---
+++ aws_default_security_group/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_cidr_block" {}
+variable "subet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "my_ip" {}
